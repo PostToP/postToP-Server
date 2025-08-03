ba207105da9c364e80f21d6fd9ef3acd34dd93cc
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-08-03 14:26:08 +0200

    feat: add authentication middleware

---
+++ src/api/middleware/auth.middelware.ts
@@ -0,0 +1,18 @@
+import { UnathorizedError } from "../../interface/errors";
+import { verifyToken } from "../../services/auth.service";
+import { Request, Response } from "express";
+
+export function authMiddleware(req: Request, res: Response, next: Function) {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        throw new UnathorizedError("Authorization token is missing");
+    }
+
+    const decoded = verifyToken(token);
+    if (!decoded.ok) {
+        throw new UnathorizedError("Invalid or expired token");
+    }
+
+    req.userID = decoded.data!.userId;
+    next();
+}
