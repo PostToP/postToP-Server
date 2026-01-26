1f977ecb5022c70bec278f38816708d21c8cad3d
GitDevla <pataki.david.ferenc@gmail.com>
2026-01-26 16:44:52 +0100

    refactor: remove commented-out getWeeklyTopStats method from UserService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,19 +2,6 @@ import { UserQueries } from "../database/queries/user.queries";
 import { InvalidUserError } from "../interface/errors";
 
 export class UserService {
-  // static async getWeeklyTopStats(userHandle: string) {
-  //     const user = await UserQueries.fetchBy(userHandle, 'handle');
-  //     if (!user) throw new InvalidUserError("User not found");
-  //     const topMusic = UserQueries.getWeeklyTopMusic(user.id);
-  //     const topArtists = UserQueries.getWeeklyTopArtists(user.id);
-  //     const topGenres = UserQueries.getWeeklyTopGenres(user.id);
-  //     return {
-  //         topMusic: await topMusic,
-  //         topArtists: await topArtists,
-  //         topGenres: await topGenres
-  //     };
-  // }
-
   static async getUserHistory(userHandle: string, filters: Partial<{ limit: number; offset: number }>) {
     const user = await UserQueries.fetchBy(userHandle, "handle");
     if (!user) throw new InvalidUserError("User not found");
